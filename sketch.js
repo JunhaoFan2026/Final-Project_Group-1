7fe20c712d046a4ac5b29f47222567bd275668d5
JunhaoFan2026 <jfan0714@uni.sydney.edu.au>
2026-06-03 23:47:18 +1000

    Create a sand-dropping simulation that forms a small hill.
    When the mouse is dragged, sand particles fall to the bottom. Newly added sand particles above can randomly roll to the left or right, gradually accumulating into a small hill-like slope.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,82 @@
+let grid;
+let w = 10;
+let cols, rows;
+
+function make2DArray(cols, rows){
+  let arr = new Array(cols);
+  for (let i = 0;  i < arr.length; i++){
+    arr[i] = new Array(rows);
+    for (let j = 0; j < arr[i].length; j++)
+      arr[i][j] = 0;
+  }
+  return arr;
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 800);
+  cols = width / w;
+  rows = height / w;
+  grid = make2DArray(cols, rows);
+
+  for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; j++){
+      grid[i][j] = 0;
+    }
+  }
+}
+
+function mouseDragged(){
+  let col = floor(mouseX / w);
+  let row = floor(mouseY / w);
+  if (col >= 0 && col <= cols - 1 && row >= 0 && row <= rows - 1){
+  grid[col][row] = 1;
+  }
 }
 
 function draw() {
   background(220);
+
+  for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; j++){
+      noStroke();
+     if (grid[i][j] == 1){
+      fill(255);
+      let x = i * w;
+      let y = j * w;
+      square(x, y, w);
+     }
+    }
+  }
+
+  let nextGrid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; j++){
+      let state = grid[i][j];
+      if (state === 1){
+        let below = grid[i][j + 1];
+
+        let dir = 1;
+        if (random(1) < 0.5) {
+          dir *= -1;
+        }
+        let belowA, belowB;
+        if (i + dir >= 0 && i + dir <= cols - 1){
+          belowA = grid[i + dir][j + 1];
+        }
+        if (i - dir >= 0 && i - dir <= cols - 1){
+          belowB = grid[i - dir][j + 1];
+        }
+        if (below === 0){
+          nextGrid[i][j + 1] = 1;
+        } else if (belowA === 0) {
+          nextGrid[i + dir][j + 1] = 1;
+        } else if (belowB === 0) {
+          nextGrid[i - dir][j + 1] = 1;
+        } else {
+          nextGrid[i][j] = 1;
+        }
+      }
+    }
+  }
+    grid = nextGrid;
 }
